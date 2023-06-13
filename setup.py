f6958006289cb3ba98187d6cf9c505c5c1f96cca
Aditya Hase <aditya@adityahase.com>
2023-06-13 14:53:59 +0530

    fix: Download and Install Chromium for Playwright

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import find_packages, setup
+from setuptools.command.develop import develop
 
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in website_builder/__init__.py
 from website_builder import __version__ as version
+
+
+class RunDevelopCommand(develop):
+	def run(self):
+		import subprocess
+		subprocess.check_output("playwright install chromium", shell=True)
+		develop.run(self)
+
 
 setup(
 	name="website_builder",
@@ -14,5 +23,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=install_requires,
+	cmdclass={'develop': RunDevelopCommand},
 )
